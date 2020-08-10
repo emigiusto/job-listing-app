af9698a749a213fb43b8bdffd61096876b792911
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-08-10 14:04:51 +0200

    finish-responsive.v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import JobBoardComponent from './assets/components/JobBoardComponent'
+import data from './assets/data.json'
 
 function App() {
+  const [jobs,setJobs] = useState([])
+
+  useEffect(()=>{
+    setJobs(data);
+  },[])
+
+
   return (
     <div className="App">
-      Hello
+      <header className="bg-teal-500 mb-12">
+          <img src="/images/bg-header-desktop.svg" alt="bg-img"></img>
+      </header>
+      {
+        jobs.lenght === 0 ? (<p>Jobs are fetching...</p>)
+        : jobs.map(job => <JobBoardComponent job={job} key={job.id} />)
+      }
     </div>
   );
 }
