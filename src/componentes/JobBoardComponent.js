ea0fe127de9f324a3eba7adff518d551bc6b829e
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-08-10 11:41:23 +0200

    first-commit

---
+++ src/componentes/JobBoardComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JobBoardComponent extends Component {
+    render() {
+        return (
+            <div>
+                Hi from JobBoardComponent
+            </div>
+        );
+    }
+}
+
+export default JobBoardComponent;
